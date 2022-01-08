fb5106c576e7817a9a5d2c7b77533e0cdc060c7e
Luiz Felipe <luizfelipedasilva678@gmail.com>
2022-01-08 19:31:59 -0300

    Memento

---
+++ src/behavioural/memento/main.ts
@@ -0,0 +1,11 @@
+import { ImageEditor } from './image-editor';
+import { ImageEditorBackupManager } from './image-editor-backup-manager';
+
+const imageEditor = new ImageEditor('/media/image.png', 'png');
+const backupManager = new ImageEditorBackupManager(imageEditor);
+
+backupManager.backup();
+imageEditor.convertFormatTo('gif');
+
+backupManager.undo();
+console.log(imageEditor);
